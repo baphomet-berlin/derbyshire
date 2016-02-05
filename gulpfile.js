2187ffdd212fdea1215b9c2444385606cba4e33f
Claudio Brandolino <cbrandolino@gmail.com>
2016-02-05 07:52:25 +0100

    Added browserify/es6/gulp support

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(
+    browserify(
+      { debug: true, extensions: ['.es6'], entries: ['src/main.es6'] }
+    ).transform(
+      babelify.configure({ extensions: ['.es6'], presets: ["es2015"] })
+    )
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
---
+++ src/main.es6
@@ -0,0 +1,8 @@
+class Derbyshire {
+
+  constructor() {
+    
+  }
+}
+
+export default Derbyshire;
